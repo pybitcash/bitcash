d12f33d4f47df13f5c84d4406911443b838569fb
Excalibur <excalibur0922@gmail.com>
2018-07-21 17:14:20 +1200

    update

--- bitcash/bitpusher/bitpusher.py
+++ bitcash/bitpusher/bitpusher.py
@@ -7,7 +7,7 @@ def create_pushdata(lst_of_pushdata):
     Returns binary encoded OP_RETURN pushdata (automatically adds intervening OP_CODES specifying number of bytes in each pushdata element)
     0x6a (i.e. OP_RETURN) is added in other, auxillary functions; only pushdata is returned.
     Max 220 bytes of pushdata
-    
+
     Parameters
     ----------
     lst_of_pushdata : array of tuples
@@ -42,13 +42,13 @@ def create_pushdata(lst_of_pushdata):
     # Import private key with bitcash
         my_key = bitcash.PrivateKey('WIF Compressed (base58) here')
         my_key.get_unspents() #necessary step --> updates my_key.unspents object variable
-        
+
     # Create lst_of_pushdata
         lst_of_pushdata =  [('6d01', 'hex'),
                             ('bitPUSHER', 'utf-8')]
 
     # Output OP_RETURN pushdata as bytes and store in variable
-        pushdata = bitpusher.create_pushdata(lst_of_pushdata)
+        pushdata = bitcash.bitpusher.create_pushdata(lst_of_pushdata)
 
     # Create rawtx ready for broadcast (fee = 1 sat/byte; sending 0.0001 BCH back to own address)
         rawtx = my_key.create_transaction([(my_key.address, 0.0001, 'bch')], fee=1, message=pushdata, custom_PUSHDATA=True)
@@ -56,7 +56,7 @@ def create_pushdata(lst_of_pushdata):
     # Broadcast rawtx
         bitcash.network.services.NetworkAPI.broadcast_tx(rawtx)
         --> look at block explorer or wallet to see new transaction!
-        
+
     ALTERNATIVE DEFAULT SETUP FOR AUTOMATING ABOVE STEPS --> See bitpusher()
     '''
     pushdata = b''
@@ -78,3 +78,40 @@ def create_pushdata(lst_of_pushdata):
         raise ValueError("Total bytes in OP_RETURN cannot exceed 220 bytes at present - apologies")
 
     return pushdata
+
+
+def rawtx(PrivateKey, pushdata, fee=1):
+
+    PrivateKey.get_unspents()
+    pushdata = bitcash.bitpusher.create_pushdata(pushdata)
+    rawtx = PrivateKey.create_transaction([(PrivateKey.address, 0.0001, 'bch')], fee=1, message=pushdata, custom_pushdata=True)
+    return rawtx
+
+
+def bitpush(PrivateKey, lst_of_pushdata, fee=1):
+    '''
+    All-in-one function for generating a rawtx with desired OP_RETURN metadata and broadcasting it to the blockchain
+
+    Parameters
+    ----------
+    PrivateKey : bitcash.PrivateKey object
+
+    pushdata : lst_of_pushdata in a 1-D array of 2-D tuples (pushdata, encoding)
+
+    fee : sat/byte (defaults to 1 satoshi per byte)
+
+    Returns
+    -------
+    broadcasts rawtx
+
+    Examples
+    --------
+    lst_of_pushdata = [('6d01','hex'), ('my_new_memocash_name', 'utf-8')]
+    bitpush(my_key, lst_of_pushdata)
+
+    '''
+    PrivateKey.get_unspents()
+    pushdata = bitcash.bitpusher.create_pushdata(lst_of_pushdata)
+    rawtx = PrivateKey.create_transaction([(PrivateKey.address, 0.0001, 'bch')], fee=1, message=pushdata, custom_pushdata=True)
+
+    return bitcash.network.services.NetworkAPI.broadcast_tx(rawtx)
