091551a32c7771a2ae32a4bc44bb662003fbf717
Corentin Mercier <corentin@mercier.link>
2021-06-18 14:14:08 +0700

    Change docs date to 2021

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'bitcash'
-copyright = '2020, BitCash'
+copyright = '2021, BitCash'
 author = 'Teran McKinney'
 
 # The version info for the project you're documenting, acts as replacement for
