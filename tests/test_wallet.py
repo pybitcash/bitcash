bb8da966d19fd68e9ca978c6c1218ec77965c9cb
nexerino <michael.tharp.3@gmail.com>
2021-09-13 13:18:45 +0700

    Adjust dependencies and fix requests-mock unittest

--- tests/test_wallet.py
+++ tests/test_wallet.py
@@ -304,7 +304,9 @@ def mocked_requests_get(*args, **kwargs):
         "child_nft_unconfirmed_inputs": MockResponse(json.loads(SLP_TESTS_CHILD_NFT_UNCONFIRMED_TYPE_65_INPUTS_RESPONSE), 200),
         "child_nft_token_details": MockResponse(json.loads(SLP_TESTS_CHILD_NFT_TOKEN_DETAILS_RESPONSE), 200),
         "nft_fanned": MockResponse(json.loads(SLP_TESTS_CHILD_NFT_TOKEN_UTXOS_RESPONSE), 200),
-        "exception": Exception
+        "exception": Exception,
+        "not_enough_fanned": MockResponse(json.loads(SLP_TESTS_CHILD_NFT_NOT_ENOUGH_FANNED_RESPONSE), 200),
+        "no_enough_fanned": MockResponse(json.loads(SLP_TESTS_CHILD_NFT_NO_FANNED_RESPONSE), 200),
         }
 
     return case.get(kwargs.get("key"))
@@ -1037,22 +1039,14 @@ class TestPrivateKeyRegtest:
             == "1d7c9076c30053e665e1768ae1b9795600e825bbb2e87334196f2418b4228ab7"
         )
 
-    def test_child_nft_not_enough_fanned(self, requests_mock):
-        requests_mock.get(
-            SLP_TESTS_CHILD_NFT_TOKEN_UTXOS_URL,
-            json=json.loads(SLP_TESTS_CHILD_NFT_NOT_ENOUGH_FANNED_RESPONSE),
-            status_code=200,
-        )
-        requests_mock.get(
-            SLP_TESTS_CHILD_NFT_UNCONFIRMED_TYPE_65_INPUTS_URL,
-            json=json.loads(SLP_TESTS_CHILD_NFT_UNCONFIRMED_TYPE_65_INPUTS_RESPONSE),
-            status_code=200,
-        )
-        requests_mock.get(
-            SLP_TESTS_CHILD_NFT_TOKEN_DETAILS_URL,
-            json=json.loads(SLP_TESTS_CHILD_NFT_TOKEN_DETAILS_RESPONSE),
-            status_code=200,
-        )
+
+    @mock.patch("requests.get")
+    def test_child_nft_not_enough_fanned(self, mock1):
+        mock1.side_effect = [
+            mocked_requests_get(key="not_enough_fanned"),
+            mocked_requests_get(key="child_nft_unconfirmed_inputs"),
+            mocked_requests_get(key="child_nft_token_details"),
+        ]
         private_key = PrivateKeyRegtest(WALLET_FORMAT_TEST)
 
         with pytest.raises(Exception) as exec:
@@ -1062,22 +1056,14 @@ class TestPrivateKeyRegtest:
 
             assert exec.value.message == "Not enough fanned group utxos."
 
-    def test_child_nft_no_fanned(self, requests_mock):
-        requests_mock.get(
-            SLP_TESTS_CHILD_NFT_TOKEN_UTXOS_URL,
-            json=json.loads(SLP_TESTS_CHILD_NFT_NO_FANNED_RESPONSE),
-            status_code=200,
-        )
-        requests_mock.get(
-            SLP_TESTS_CHILD_NFT_UNCONFIRMED_TYPE_65_INPUTS_URL,
-            json=json.loads(SLP_TESTS_CHILD_NFT_UNCONFIRMED_TYPE_65_INPUTS_RESPONSE),
-            status_code=200,
-        )
-        requests_mock.get(
-            SLP_TESTS_CHILD_NFT_TOKEN_DETAILS_URL,
-            json=json.loads(SLP_TESTS_CHILD_NFT_TOKEN_DETAILS_RESPONSE),
-            status_code=200,
-        )
+
+    @mock.patch("requests.get")
+    def test_child_nft_no_fanned(self, mock1):
+        mock1.side_effect = [
+            mocked_requests_get(key="no_enough_fanned"),
+            mocked_requests_get(key="child_nft_unconfirmed_inputs"),
+            mocked_requests_get(key="child_nft_token_details"),
+        ]
         private_key = PrivateKeyRegtest(WALLET_FORMAT_TEST)
 
         with pytest.raises(Exception) as exec:
